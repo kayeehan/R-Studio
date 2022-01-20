4775e3497668a6622d2610492d67a83d78644da5
Kyeongmin Han <hkmhcm@gmail.com>
2022-01-20 21:35:04 +0900

    first commit

--- R_studio_prac.R
+++ R_studio_prac.R
@@ -1,12 +1,44 @@
-#22.01.20 R test
+employees<-read.csv('C:/data_bigdata/emp_mod.csv',header=T)
+departments<-read.csv('C:/data_bigdata/dept.csv',header=T)
+sales<-read.csv('C:/data_bigdata/fruits_sales.csv',header=T)
+install.packages('plyr')
+library(plyr)
+install.packages("dplyr")
+library(dplyr)
 install.packages("sqldf")
 library(sqldf)
-employees<-read.csv('C:/data_bigdata/emp_mod.csv',header=T)
-#[문제151]동일한 날짜에 입사한 사원들의 정보를 출력해주세요.(sqldf를 이용하세요)
-sqldf('select e.*
-      from employees e
-      where exists (select "x"
-                    from employees
-                    where hire_date=e.hire_date
-                    and employee_id<>e.employee_id)')
-#뭐지
+
+#22.01.20
+#문제] COMMISSION_PCT가 NA인 사원들 급여 평균과 
+#COMMISSION_PCT가 NA가 아닌 사원들의 급여 평균 차를 구하세요
+
+x<-employees[is.na(employees$COMMISSION_PCT),'SALARY']
+y<-employees[!is.na(employees$COMMISSION_PCT),'SALARY']
+mean(x,na.rm=T)-mean(y,na.rm=T)
+#SQL
+sqldf('select avg(e1.salary)-avg(e2.avg_sal)
+      from (select avg(salary) avg_sal
+            from employees
+            where COMMISSION_PCT is not null) e2, employees e1
+      where COMMISSION_PCT is null')
+
+#문제] COMMISSION_PCT가 NA인 사원들 중 급여가 제일 낮은 사람의 정보를 출력하세요.
+employees%>%
+  dplyr::filter(is.na(COMMISSION_PCT))%>%
+  dplyr::mutate(rank=dense_rank(SALARY))%>%
+  dplyr::filter(rank==1)
+
+#sql
+sqldf('select *
+      from employees
+      where commission_pct is null
+      and salary = (select min(salary)
+                          from employees
+                          where commission_pct is null)')
+
+
+
+
+
+
+
